5a4a9946b5c1143ec55a5ad015c90502bb5e9cdb
pedrolgr <pedrolgr47@gmail.com>
2025-12-27 23:28:12 -0300

    count button exercise

--- src/studies/02usestate-hook/App2.js
+++ src/studies/02usestate-hook/App2.js
@@ -4,14 +4,24 @@ export function App2() {
 
     const [state, setState] = useState(0)
 
-    function handleClick() {
+    function handleClickPlus() {
         setState(state+1);
+    }
+
+    function handleClickMinus() {
+        if(state-1 < 0) {
+            return setState(state)
+        } else {
+            setState(state-1);
+        }
+        
     }
 
     return(
         <>
             <h1>{state}</h1>
-            <button onClick={handleClick}>Click</button>
+            <button onClick={handleClickPlus}>+</button>
+            <button onClick={handleClickMinus}>-</button>
         </>
     )
 }
