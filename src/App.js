aebfd661b52b0c679833e81cf18c0d081d709caa
pedrolgr <pedrolgr47@gmail.com>
2025-12-29 23:22:49 -0300

    first study of conditional rendering

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import './App.css';
 import { App1 } from './studies/01creating-nested-components-passing-props/App1.js'
 import { App2 } from './studies/02usestate-hook/App2.js';
 import { App3 } from './studies/02usestate-hook/App3.js';
+import { App4 } from './studies/03conditional-rendering/App4.js';
 
 function App() {
   return (
     <div className="App">
-      <App3 />
+      <App4 state={true}/>
     </div>
   );
 }
---
+++ src/studies/03conditional-rendering/App4.js
@@ -0,0 +1,8 @@
+export function App4({state}) {
+
+    if(state){
+        return(<h1>true</h1>)
+    } else {
+        return(<h2>false</h2>)
+    }
+}
