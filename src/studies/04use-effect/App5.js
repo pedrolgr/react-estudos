12ee4112e7a088f7a18e9b6dcbf1131cbcfdd069
pedrolgr <pedrolgr47@gmail.com>
2026-01-12 23:14:06 -0300

    changed useffec code, adding button to refresh the list

--- src/studies/04use-effect/App5.js
+++ src/studies/04use-effect/App5.js
@@ -3,15 +3,24 @@ import { useEffect, useState } from "react"
 export function App5() {
 
     const [posts, setPosts] = useState([]);
+    const [rand, setRand] = useState(0);
+    
+    const handleClick = () => {
+        setRand(Math.floor(Math.random() * (15 - 3 + 1)) + 3);
+    }
 
     useEffect(() => {
-        fetch("https://jsonplaceholder.typicode.com/posts")
+        fetch(`https://jsonplaceholder.typicode.com/posts?_limit=${rand}`)
         .then(response => response.json())
         .then(data => setPosts(data))
-    }, [posts])
+    }, [rand])
+
+    
 
     return(
         <>
+        <button onClick={handleClick}>update</button>
+
         {posts.map(post => 
             <li key={post.id}>
                 {post.title}
